b0ee2a72bebc027b813933062728da217f5b7efe
Jucimara Fortini <jucimaraafortini@gmail.com>
2024-06-04 21:43:03 -0300

    Added hover on booking button

--- src/components/Header/components/BookingButton.tsx
+++ src/components/Header/components/BookingButton.tsx
@@ -1,7 +1,7 @@
 export function BookingButton(): JSX.Element {
   return (
     <>
-      <button className="px-6 py-3 xl:px-8 xl:py-3 whitespace-nowrap bg-teal-400 text-slate-100 font-semibold rounded-full text-xs xl:text-sm">
+      <button className="transition duration-200 hover:opacity-80 px-6 py-3 xl:px-8 xl:py-3 whitespace-nowrap bg-teal-400 text-slate-100 font-semibold rounded-full text-xs xl:text-sm">
         Reserve uma visita
       </button>
     </>
