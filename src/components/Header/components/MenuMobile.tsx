50875295a67283d8edafb755c5971cb4bf14cc14
Jucimara Fortini <jucimaraafortini@gmail.com>
2024-06-04 21:38:17 -0300

    Refactored header and added shadow

--- src/components/Header/components/MenuMobile.tsx
+++ src/components/Header/components/MenuMobile.tsx
@@ -3,7 +3,7 @@ import { MenuButton } from "./MenuButton";
 
 export function MenuMobile(): JSX.Element {
   return (
-    <div className="p-8 flex-col space-y-10 bg-teal-50 rounded-b-lg">
+    <div className="p-8 flex-col space-y-14 bg-teal-50 rounded-b-lg">
       <div className="flex-col space-y-4">
         <MenuButton title="home" route="/" />
         <MenuButton title="sobre" route="/about" />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,25 +7,28 @@ import { List } from "phosphor-react";
 
 export function Header(): JSX.Element {
   const [isDrawerMenuOpen, setIsDrawerMenuOpen] = useState(false);
+  const personalShadow = 'shadow-[0_16px_32px_-16px_rgba(58,135,118,0.1)]'
   return (
-    <>
-      <div className="bg-teal-50 px-8 lg:px-16 py-2 flex justify-between items-center z-0">
-        <Link href="/">
-          <img src="/images/logo.svg" alt="logo" className="h-14 w-24 object-contain" />
-        </Link>
-        <MenuDropdown />
-        <div className="hidden md:block">
-          <BookingButton />
+    <div className="absolute w-full">
+      <div className={`relative bg-teal-50 px-8 lg:px-16 py-2 flex justify-center items-center z-10 ${personalShadow}`}>
+        <div className="flex w-full max-w-screen-xl items-center justify-between">
+          <Link href="/">
+            <img src="/images/logo.svg" alt="logo" className="h-14 w-24 object-contain" />
+          </Link>
+          <MenuDropdown />
+          <div className="hidden md:block">
+            <BookingButton />
+          </div>
+          <button className="block md:hidden" onClick={() => setIsDrawerMenuOpen(value => !value)}>
+            <List size={32} color="#475569" />
+          </button>
         </div>
-        <button className="block md:hidden" onClick={() => setIsDrawerMenuOpen(value => !value)}>
-          <List size={32} color="#475569" />
-        </button>
       </div>
-      <div className={`absolute md:hidden -z-10 right-0 bg-slate-100
+      <div className={`absolute md:hidden right-0 bg-slate-100
         ${isDrawerMenuOpen ? 'animate-slide_open' : 'animate-slide_close'}`}
       >
         <MenuMobile />
       </div>
-    </>
+    </div>
   );
 };
