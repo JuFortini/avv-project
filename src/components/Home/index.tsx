2a3c4cb4ffe1d841e48f95aaa6d2b792436e2e81
Jucimara Fortini <jucimaraafortini@gmail.com>
2024-06-05 18:28:05 -0300

    Added responsive presentation on home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,18 +1,24 @@
 export function HomePage(): JSX.Element {
   return (
     <>
-
-      <div className="bg-teal-50 px-8 lg:px-16 py-2 h-svh w-full flex justify-center items-center">
-        <div className="flex justify-center items-center max-w-screen-xl w-full">
-          <div>
-            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
-            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, quia nesciunt? Vitae quia neque officiis magnam nisi repellat. Corporis tempore similique aperiam culpa! Et, impedit laudantium. Reprehenderit et eius repellendus.</h2>
+      <div className="bg-teal-50 px-8 lg:px-16 py-24 lg:py-2 h-svh w-full flex justify-center items-center">
+        <div className="flex flex-col lg:flex-row justify-center items-center max-w-[30rem] lg:max-w-screen-xl h-full w-full sm:space-y-10 space-x-20">
+          <div className="w-full max-w-[34rem] space-y-4 sm:space-y-10">
+            <h1 className="text-3xl sm:text-5xl xl:text-6xl font-serif">Lorem ipsum dolor sit, amet consec tetur.</h1>
+            <h2 className="text-sm sm:text-base xl:text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, quia nesciunt? Vitae quia neque officiis magnam nisi repellat. Corporis tempore similique aperiam culpa! Et, impedit laudantium. Reprehenderit et eius repellendus.</h2>
           </div>
-          <img
-            className="max-w-md"
-            src="https://images.unsplash.com/photo-1529030168380-b62c39672668?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-          />
+          <div className="relative w-full lg:max-w-full max-w-[30rem] h-3/5 lg:h-4/5">
+            <img
+              className="absolute object-cover right-0 -bottom-8 w-[calc(100%-2rem)] h-[calc(100%-2rem)] rounded-2xl"
+              src="https://images.unsplash.com/photo-1660721858662-9ad9f37447f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+            <img
+              className="absolute object-cover bottom-0 left-0 w-[calc(100%-2rem)] h-[calc(100%-2rem)] rounded-2xl"
+              src="https://images.unsplash.com/photo-1512099053734-e6767b535838?q=80&w=1928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </>
