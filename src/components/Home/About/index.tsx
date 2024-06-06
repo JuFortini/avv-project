5c4df64d53df3979c162aaf8233a2dfafc7d521d
Jucimara Fortini <jucimaraafortini@gmail.com>
2024-06-06 18:08:07 -0300

    Added about section idea on home page

---
+++ src/components/Home/About/index.tsx
@@ -0,0 +1,32 @@
+export function About(): JSX.Element {
+  return (
+    <>
+      <div className="flex h-svh flex-col items-center justify-center px-8 lg:px-16 py-24 lg:py-8 space-y-20">
+        <div className="space-y-4 max-w-3xl text-center">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta saepe, eligendi consectetur
+            nostrum atque temporibus dolore rerum, aliquam cum similique necessitatibus at voluptatem
+            placeat illo non, nulla eos dignissimos quis. Lorem ipsum dolor, sit amet consectetur adipisicing
+            elit. Expedita porro saepe est recusandae, dolorum corrupti praesentium eius cupiditate consectetur
+            ut blanditiis quisquam inventore dolore, iste commodi culpa autem, obcaecati nesciunt?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aperiam obcaecati atque veniam
+            quibusdam hic autem? Itaque eius libero molestiae hic asperiores laboriosam earum? Inventore
+            repellat ut fugit rerum itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente sunt impedit est nostrum quaerat
+            quia non sequi earum nam unde ducimus, veniam corporis?
+          </p>
+        </div>
+        <div className="w-1/4">
+          <div className="flex flex-col justify-center bg-rose-200 text-center font-serif h-full rounded-2xl space-y-4 py-8">
+            <h1 className="text-6xl">456+</h1>
+            <p>Lorem ipsum</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
