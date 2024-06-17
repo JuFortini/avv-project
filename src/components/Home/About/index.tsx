a6cd0325f564eb4c9ac8b597fbb01d0dfa9778d4
Jucimara Fortini <jucimaraafortini@gmail.com>
2024-06-17 16:37:02 -0300

    Refactored Home

--- src/components/Home/About/index.tsx
+++ src/components/Home/About/index.tsx
@@ -2,32 +2,30 @@ import { Achievements, AchievementsColor } from "./components/Achievements"
 
 export function About(): JSX.Element {
   return (
-    <>
-      <div className="flex flex-col items-center justify-between 2xl:h-svh px-8 lg:px-16 py-8 xl:py-24 gap-y-20 bg-teal-50">
-        <div className="space-y-4 max-w-3xl text-center">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta saepe, eligendi consectetur
-            nostrum atque temporibus dolore rerum, aliquam cum similique necessitatibus at voluptatem
-            placeat illo non, nulla eos dignissimos quis. Lorem ipsum dolor, sit amet consectetur adipisicing
-            elit. Expedita porro saepe est recusandae, dolorum corrupti praesentium eius cupiditate consectetur
-            ut blanditiis quisquam inventore dolore, iste commodi culpa autem, obcaecati nesciunt?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aperiam obcaecati atque veniam
-            quibusdam hic autem? Itaque eius libero molestiae hic asperiores laboriosam earum? Inventore
-            repellat ut fugit rerum itaque.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente sunt impedit est nostrum quaerat
-            quia non sequi earum nam unde ducimus, veniam corporis?
-          </p>
-        </div>
-        <div className="w-full flex flex-col md:flex-row justify-center items-center gap-y-4 md:gap-x-20 max-w-5xl">
-          <Achievements color={AchievementsColor.Rose} title="456+" subtitle="Lorem ipsum" />
-          <Achievements color={AchievementsColor.Teal} title="99%" subtitle="Lorem dolor" />
-          <Achievements color={AchievementsColor.Fuchsia} title="40M" subtitle="Lorem iste" />
-        </div>
+    <section className="flex flex-col items-center justify-between 2xl:h-svh px-8 lg:px-16 py-8 xl:py-24 gap-y-20 bg-teal-50">
+      <div className="space-y-4 max-w-3xl text-center">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta saepe, eligendi consectetur
+          nostrum atque temporibus dolore rerum, aliquam cum similique necessitatibus at voluptatem
+          placeat illo non, nulla eos dignissimos quis. Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Expedita porro saepe est recusandae, dolorum corrupti praesentium eius cupiditate consectetur
+          ut blanditiis quisquam inventore dolore, iste commodi culpa autem, obcaecati nesciunt?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aperiam obcaecati atque veniam
+          quibusdam hic autem? Itaque eius libero molestiae hic asperiores laboriosam earum? Inventore
+          repellat ut fugit rerum itaque.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente sunt impedit est nostrum quaerat
+          quia non sequi earum nam unde ducimus, veniam corporis?
+        </p>
       </div>
-    </>
+      <div className="w-full flex flex-col md:flex-row justify-center items-center gap-y-4 md:gap-x-20 max-w-5xl">
+        <Achievements color={AchievementsColor.Rose} title="456+" subtitle="Lorem ipsum" />
+        <Achievements color={AchievementsColor.Teal} title="99%" subtitle="Lorem dolor" />
+        <Achievements color={AchievementsColor.Fuchsia} title="40M" subtitle="Lorem iste" />
+      </div>
+    </section>
   )
 }
--- src/components/Home/Exams/index.tsx
+++ src/components/Home/Exams/index.tsx
@@ -6,45 +6,43 @@ import { GlassesPrescriptionIcon } from "./images/GlassesPrescriptionIcon";
 
 export function Exams(): JSX.Element {
   return (
-    <>
+    <section className="bg-teal-50 px-8 lg:px-16 py-16 2xl:h-svh w-full flex flex-col justify-between items-center">
       {/* // TODO: implementar redirecionamentos */}
-      <div className="bg-teal-50 px-8 lg:px-16 py-16 2xl:h-svh w-full flex flex-col justify-between items-center">
-        <h1 className="font-serif font-semibold text-4xl p-8">Nossos exames</h1>
-        <div className="flex flex-col items-center lg:items-start justify-start gap-y-8 w-full">
-          <div className="flex flex-col lg:flex-row self-center gap-y-8 lg:gap-x-20">
-            <ExamCard
-              icon={<ColorBlindnessIcon />}
-              name="Examescopia"
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quibusdam tempore
+      <h1 className="font-serif font-semibold text-4xl p-8">Nossos exames</h1>
+      <div className="flex flex-col items-center lg:items-start justify-start gap-y-8 w-full">
+        <div className="flex flex-col lg:flex-row self-center gap-y-8 lg:gap-x-20">
+          <ExamCard
+            icon={<ColorBlindnessIcon />}
+            name="Examescopia"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quibusdam tempore
             dolorem unde animi consequatur corporis fugiat nobis illum, labore, cum soluta quam harum voluptate,
             officiis quia laborum adipisci voluptas."
-              action="/"
-            />
-            <ExamCard
-              icon={<GlassesPrescriptionIcon />}
-              name="Examescopia"
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae omnis tempore,
+            action="/"
+          />
+          <ExamCard
+            icon={<GlassesPrescriptionIcon />}
+            name="Examescopia"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae omnis tempore,
               possimus tenetur veniam eveniet magni quo necessitatibus, quisquam quam illum quae a?
               Eius laborum a fuga quia voluptate ex."
-              action="/"
-            />
-            <ExamCard
-              icon={<FundusMapIcon />}
-              name="Examescopia"
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, earum deserunt?
+            action="/"
+          />
+          <ExamCard
+            icon={<FundusMapIcon />}
+            name="Examescopia"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, earum deserunt?
               Exercitationem esse ducimus beatae quia accusantium velit modi quae sapiente deleniti eveniet
               quibusdam sequi laboriosam natus minus, nemo ipsum!"
-              action="/"
-            />
-          </div>
-          <Link
-            className="rounded bg-teal-400 transition duration-200 hover:opacity-80 w-min text-nowrap py-2 px-4 text-sm text-slate-50 font-semibold"
-            href="/exams"
-          >
-            Ver todos
-          </Link>
+            action="/"
+          />
         </div>
+        <Link
+          className="rounded bg-teal-400 transition duration-200 hover:opacity-80 w-min text-nowrap py-2 px-4 text-sm text-slate-50 font-semibold"
+          href="/exams"
+        >
+          Ver todos
+        </Link>
       </div>
-    </>
+    </section>
   );
 }
